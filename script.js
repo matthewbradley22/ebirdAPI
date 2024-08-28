0cc481f9263908eea0c721bd6fc6ed9e76ade0a4
matthewbradley22 <mbradley22@utexas.edu>
2024-08-28 16:21:17 -0500

    error message

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 const stateSubmit = document.getElementById("submit")
 const stateCode = document.getElementById("state")
+const warning = document.getElementById("wrong")
 
 var myHeaders = new Headers();
 myHeaders.append("X-eBirdApiToken", "q7m6fj472qht");
@@ -13,12 +14,23 @@ var requestOptions = {
 
 
 async function getBirdDat(location) {
-  const url = "https://api.ebird.org/v2/data/obs/" + location + "/recent"
-  const response = await fetch(url, requestOptions)
-
-  const data = await response.json()
-  showNames(data)
-
+  try {
+    const url = "https://api.ebird.org/v2/data/obs/" + location + "/recent"
+    const response = await fetch(url, requestOptions)
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`);
+    }
+    const data = await response.json()
+    if(Object.keys(data).length === 0){
+      wrong.innerHTML = "No such place"
+    }else{
+      wrong.innerHTML = ""
+    }
+    showNames(data)
+  }
+  catch (error) {
+    console.error(`Could not get products: ${error}`);
+  }
 }
 
 function showNames(recentBirds) {
