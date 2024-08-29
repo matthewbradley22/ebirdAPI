ebec19d086fd521fb61078d5e9a5efa3af6adb04
matthewbradley22 <mbradley22@utexas.edu>
2024-08-29 12:48:37 -0500

    map implementation

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 
-const stateSubmit = document.getElementById("submit")
-const stateCode = document.getElementById("state")
-const warning = document.getElementById("wrong")
+//Get values from html page such as user chosen state or type of 
+//filtering they wish to use
+const stateSubmit = document.getElementById("submit");
+const stateCode = document.getElementById("state");
+const warning = document.getElementById("wrong");
+let userChoice = document.getElementById("typeFilter");
+
+//create map object for user to select lat/long 
 let map = L.map('map').setView([30.6, -96], 13);
+
+//Create click function that I will change later to pass to ebird api
+let popup = L.popup()
+map.on('click', onMapClick);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+//Interact with ebird api
 var myHeaders = new Headers();
 myHeaders.append("X-eBirdApiToken", "q7m6fj472qht");
 
@@ -17,6 +27,11 @@ var requestOptions = {
   headers: myHeaders,
   redirect: 'follow'
 };
+
+//intermediate function that I will change to change page state
+userChoice.addEventListener("change", (event) =>{
+  console.log(event.target.value)
+})
 
 
 async function getBirdDat(location) {
@@ -58,3 +73,10 @@ stateSubmit.addEventListener("click", function () {
   const regionString = "US-" + stateCode.value.toUpperCase()
   getBirdDat(regionString)
 })
+
+function onMapClick(e) {
+  popup
+      .setLatLng(e.latlng)
+      .setContent("You clicked the map at " + e.latlng.toString())
+      .openOn(map);
+}
